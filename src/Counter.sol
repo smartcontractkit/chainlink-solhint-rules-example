8d514c322c6d5d8c1ea77b1856e93a2b51129910
Yos Riady <yos.riady@smartcontract.com>
2022-10-19 20:03:17 +0800

    More exhaustive example

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+contract CounterStorage {
+    uint256 foo;
+
+    constructor(uint256 _foo) {
+        foo = _foo;
+    }
+}
+
+contract Counter is CounterStorage(123) {
+    // solhint-disable-next-line
+    uint256 private constant hello;
+
+    // solhint-disable-next-line
     uint256 private number;
 
     function setNumber(uint256 newNumber) public {
+        // solhint-disable-next-line
         require(newNumber != 0);
         number = newNumber;
     }
 
+    // solhint-disable-next-line
     function increment() internal {
         number++;
     }
